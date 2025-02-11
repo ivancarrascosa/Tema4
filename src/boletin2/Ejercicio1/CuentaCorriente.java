7b68037718348ddb16e710c079c7cf3e0d1971a9
ivancarrascosa <ivan.carrascosa@iesnervion.es>
2025-02-11 14:25:39 +0100

    Clase cuentas corrientes

---
+++ src/boletin2/Ejercicio1/CuentaCorriente.java
@@ -0,0 +1,171 @@
+package boletin2.Ejercicio1;
+
+/**
+ * Clase para almacenar las cuentas corrientes de las personas con las distintas
+ * operaciones que se pueden realizar con estas.
+ */
+public class CuentaCorriente {
+	// creo un enumerado con las dos naciones que pueden estar
+	enum NACION {
+		ESPAÑOLA, EXTRANJERA
+	}
+
+	/**
+	 * DNI del dueño de la cuenta
+	 */
+	private String DNI;
+	/**
+	 * Nombre del dueño de la persona
+	 */
+	private String nombre = "";
+	/**
+	 * Saldo en la cuenta de la persona
+	 */
+	private float saldo;
+	/**
+	 * Nacionalidad de la persona
+	 */
+	private NACION nacionalidad = NACION.ESPAÑOLA;
+
+	/**
+	 * Constructor con DNI y saldo
+	 * 
+	 * @param DNI   DNI de la persona
+	 * @param saldo saldo de la persona
+	 */
+	public CuentaCorriente(String DNI, float saldo) {
+		if (DNI != null && !DNI.isBlank()) {
+			this.DNI = DNI;
+		}
+
+		this.saldo = saldo;
+	}
+
+	/**
+	 * Constructor con DNI, nombre y saldo
+	 * 
+	 * @param DNI    DNI de la persona
+	 * @param nombre nombre de la persona
+	 * @param saldo  saldo de la persona
+	 */
+	public CuentaCorriente(String DNI, String nombre, float saldo) {
+		this.DNI = DNI;
+		this.nombre = nombre;
+		this.saldo = saldo;
+
+	}
+	/**
+	 * Constructor con todos los atributos de la clase
+	 * @param DNI DNI de la persona
+	 * @param nombre nombre de la persona
+	 * @param saldo saldo de la persona
+	 * @param nacionalidad nacionalidad de la persona
+	 */
+	public CuentaCorriente(String DNI, String nombre, float saldo, String nacionalidad) {
+		// Lo paso a mayúscula para que entre en el enumerado aunqu esté en minusculas
+		nacionalidad = nacionalidad.toUpperCase();
+		if (DNI != null && !DNI.isBlank()) {
+			this.DNI = DNI;
+		}
+		if (nombre != null && !nombre.isBlank()) {
+			this.nombre = nombre;
+		}
+		this.saldo = saldo;
+		switch (nacionalidad) {
+		case "ESPAÑOLA":
+			this.nacionalidad = NACION.ESPAÑOLA;
+			break;
+		case "EXTRANJERA":
+			this.nacionalidad = NACION.EXTRANJERA;
+			break;
+		default:
+			
+			break;
+		}
+	}
+	/**
+	 * getter para obtener el DNI 
+	 * @return DNI de la persona
+	 */
+	public String getDNI() {
+		return DNI;
+	}
+	/**
+	 * getter para obtener el nombre
+	 * @return nombre de la persona
+	 */
+	public String getNombre() {
+		return nombre;
+	}
+	/**
+	 * setter para cambiar el nombre de una persona
+	 * @param nombre nombre que le queremos poner a la persona
+	 */
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	/**
+	 * getter para el saldo de la persona
+	 * @return saldo de la persona
+	 */
+	public float getSaldo() {
+		return saldo;
+	}
+	/**
+	 * Getter para la nacionalidad de la persona 
+	 * @return nacionalidad de la persona
+	 */
+	public NACION getNacionalidad() {
+		return nacionalidad;
+	}
+	/**
+	 * Método para sacar dinero de una cuenta corriente
+	 * @param dinero dinero que queremos sacar de la cuenta
+	 * @return true si hay suficiente dinero en la cuenta, false si no hay suficiente
+	 */
+	public boolean sacarDinero(float dinero) {
+		boolean res = true;
+		if (this.saldo < dinero) {
+			res = false;
+		} else {
+			this.saldo -= dinero;
+		}
+		return res;
+	}
+	/**
+	 * Método para ingresar dinero en una cuenta corriente
+	 * @param dinero dinero que queremos ingresar en la cuenta
+	 * @return true si la cantidad que queremos ingresar es positiva, false si no lo es
+	 */
+	public boolean ingresarDinero(float dinero) {
+		boolean res = true;
+		if (dinero <= 0) {
+			res = false;
+		} else {
+			this.saldo += dinero;
+		}
+		return res;
+	}
+	/**
+	 * Método to String que devuelve un string con todos los atributos del objeto
+	 * @return todos los atributos del objeto
+	 */
+	@Override
+	public String toString() {
+		return "DNI=" + DNI + ", nombre=" + nombre + ", saldo=" + saldo + "€, nacionalidad="
+				+ nacionalidad;
+	}
+	/**
+	 * Método equals que compara los objetos
+	 * @return si el DNI y el nombre son iguales
+	 */
+	@Override
+	public boolean equals(Object obj) {
+		CuentaCorriente other = (CuentaCorriente) obj;
+		return this.DNI.equals(other.DNI) && this.nombre.equals(other.nombre);
+	}
+	
+	
+	
+	
+}
--- src/boletin2/Ejercicio1/MainCuentaCorriente.java
+++ src/boletin2/Ejercicio1/MainCuentaCorriente.java
@@ -3,8 +3,15 @@ package boletin2.Ejercicio1;
 public class MainCuentaCorriente {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		CuentaCorriente c1 = new CuentaCorriente("54932030C","Iván", 500, "Española" );
+		CuentaCorriente c2 = new CuentaCorriente("54932030C","Iván", 5000, "Española" );
+		
+		System.out.println(c1.equals(c2));
+		c1.ingresarDinero(5);
+		System.out.println(c1.getSaldo());
+		System.out.println(c1.sacarDinero(100000));
+		c1.sacarDinero(405);
+		System.out.println(c1.getSaldo());
 	}
 
 }
